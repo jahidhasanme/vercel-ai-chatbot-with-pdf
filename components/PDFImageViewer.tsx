30842f5ef20bc4707a367cdd931f470426f275d4
Jahid Hasan <jahidsite0@gmail.com>
2025-03-14 22:25:18 +0600

    Remove PDF image viewer component and related dependencies

--- components/PDFImageViewer.tsx
+++ components/PDFImageViewer.tsx
@@ -1,26 +1,26 @@
-"use client";
+// "use client";
 
-import React from "react";
-import { Worker, Viewer } from "@react-pdf-viewer/core";
-import { thumbnailPlugin } from "@react-pdf-viewer/thumbnail";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import "@react-pdf-viewer/thumbnail/lib/styles/index.css";
+// import React from "react";
+// import { Worker, Viewer } from "@react-pdf-viewer/core";
+// import { thumbnailPlugin } from "@react-pdf-viewer/thumbnail";
+// import "@react-pdf-viewer/core/lib/styles/index.css";
+// import "@react-pdf-viewer/thumbnail/lib/styles/index.css";
 
-interface PDFImageViewerProps {
-    pdfUrl: string;
-    style?: React.CSSProperties;
-}
+// interface PDFImageViewerProps {
+//     pdfUrl: string;
+//     style?: React.CSSProperties;
+// }
 
-const PDFImageViewer: React.FC<PDFImageViewerProps> = ({ pdfUrl, style }) => {
-    const thumbnailPluginInstance = thumbnailPlugin();
+// const PDFImageViewer: React.FC<PDFImageViewerProps> = ({ pdfUrl, style }) => {
+//     const thumbnailPluginInstance = thumbnailPlugin();
 
-    return (
-        <div style={style}>
-            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-                <Viewer fileUrl={pdfUrl} plugins={[thumbnailPluginInstance]} />
-            </Worker>
-        </div>
-    );
-};
+//     return (
+//         <div style={style}>
+//             <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+//                 <Viewer fileUrl={pdfUrl} plugins={[thumbnailPluginInstance]} />
+//             </Worker>
+//         </div>
+//     );
+// };
 
-export default PDFImageViewer;
+// export default PDFImageViewer;
